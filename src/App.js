23e2fb5156ac113e9e09580f58a348d25d6a619d
Viktoravanesov <el.eng1989@rambler.ru>
2024-02-12 18:43:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Getintouch } from "./ui/organisms/Getintouch";
+import { About } from "./ui/organisms/about";
+import { Footer } from "./ui/organisms/footer";
+import { Header } from "./ui/organisms/header";
+import { Main } from "./ui/organisms/main";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ <>
+    <Header />
+    <Main />
+    <About />
+    <Getintouch />
+    <Footer />
+
+</>
+    );
 }
 
 export default App;
